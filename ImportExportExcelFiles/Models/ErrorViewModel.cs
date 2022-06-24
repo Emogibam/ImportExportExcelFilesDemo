da7ca8145bd6be8a00bd4ebb92178cfeca168e73
Emogibam <88818922+Emogibam@users.noreply.github.com>
2022-06-24 10:20:20 +0100

    first commit

---
+++ ImportExportExcelFiles/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImportExportExcelFiles.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
